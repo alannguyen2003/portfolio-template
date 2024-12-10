297eb947ad93ef792298cf317cdbf62b4366839e
alannguyen2003 <nguyenho30112003@gmail.com>
2024-12-10 22:59:55 +0700

    add hobbies page

--- js/app.js
+++ js/app.js
@@ -12,11 +12,79 @@ const forwardBtn = document.querySelector(".forward-btn");
 const backwardBtn = document.querySelector(".backward-btn");
 
 playBtn.addEventListener('click', () => {
+    if (playBtn.className.includes('pause')){
+        music.play();
+    } else {
+        music.pause();
+    }
     playBtn.classList.toggle('pause');
     disk.classList.toggle('play')
 });
 
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    disk.classList.add('play');
+}
+
 const setMusic = (i) => {
     seekBar.value = 0;
     let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}`;
+
+    currentTime.innerHTML = '00:00';
+    seekBar.max = music.duration;
+    console.log(music.duration);
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        console.log(music.duration);
+        musicDuration.innerHTML = formatTime(music.duration);
+    }, 300);
 }
+
+setMusic(0);
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0${min}`
+    }
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0${sec}`
+    }
+
+    return `${min} : ${sec}`;
+}
+
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime)
+}, 500);
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+})
+
+forwardBtn.addEventListener('click', () => {
+    if (currentMusic >= songs.length - 1) {
+        currentMusic = 0;
+    } else {
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
+backwardBtn.addEventListener('click', () => {
+    if (currentMusic <= 0) {
+        currentMusic = songs.length - 1;
+    } else {
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
--- js/data.js
+++ js/data.js
@@ -1,32 +1,8 @@
 let songs = [
     {
-        name: 'song 1',
-        path: 'musics/Song 1.mp3',
-        artist: 'artist 1',
+        name: 'Life Is Still Going On',
+        path: 'musics/Life Is Still Going On.mp3',
+        artist: 'NCT DREAM',
         cover: 'images/cover 1.png'
-    },
-    {
-        name: 'song 2',
-        path: 'musics/Song 2.mp3',
-        artist: 'artist 2',
-        cover: 'images/cover 2.png'
-    },
-    {
-        name: 'song 3',
-        path: 'musics/Song 3.mp3',
-        artist: 'artist 3',
-        cover: 'images/cover 3.png'
-    },
-    {
-        name: 'song 4',
-        path: 'musics/Song 4.mp3',
-        artist: 'artist 4',
-        cover: 'images/cover 4.png'
-    },
-    {
-        name: 'song 5',
-        path: 'musics/Song 5.mp3',
-        artist: 'artist 5',
-        cover: 'images/cover 5.png'
-    },
+    }
 ]
