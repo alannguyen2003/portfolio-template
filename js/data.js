a2a5136110a99be3cf198b2a71e6a10be5ec012a
alannguyen2003 <nguyenho30112003@gmail.com>
2024-12-10 23:23:03 +0700

    fix link music

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 let songs = [
     {
         name: 'Life Is Still Going On',
-        path: 'musics/Life Is Still Going On.mp3',
+        path: 'https://res.cloudinary.com/ds8cv8hcq/video/upload/v1733847742/Life_Is_Still_Going_On_pkbvxo.mp3',
         artist: 'NCT DREAM',
         cover: 'images/cover 1.png'
     }
